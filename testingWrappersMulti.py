a9d2acad57e702a117f197b7790a20db9c564c7c
orangeduice <osjack.oj@gmail.com>
2024-10-04 17:08:09 +0100

    Added file to test multiple required grad inputs.

---
+++ testingWrappersMulti.py
@@ -0,0 +1,92 @@
+from scipy.special import spherical_jn  # , spherical_yn
+
+# from scipy import special
+
+import torch
+from torch.autograd import Function
+# from torch.nn.modules.module import Module
+# from torch.nn.parameter import Parameter
+
+# import matplotlib.pyplot as plt
+import numpy as np
+
+
+class torch_jn(Function):
+    @staticmethod
+    def forward(ctx, input, n):
+        input_np = input.detach().numpy()
+        result = torch.from_numpy(spherical_jn(n, input_np))
+        ctx.save_for_backward(input)
+        ctx.n = n  # Save n (non-learnable) in ctx
+
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, = ctx.saved_tensors
+        grad_output = grad_output.detach().numpy()
+
+        # Derivative of spherical_jn w.r.t. input
+        input_np = input.detach().numpy()
+        grad_input = spherical_jn(ctx.n, input_np, derivative=True)
+
+        # In automatic differentiation, the `grad_output` tensor
+        # represents the gradient of the loss with respect to the output of the
+        # function. To obtain the gradient of the loss with respect to the
+        # input, you need to multiply this `grad_output` by the local
+        # derivative of your function with respect to the input, which is
+        # represented by `grad_input`.
+        grad_input = torch.from_numpy(grad_input) \
+            * torch.from_numpy(grad_output)
+
+        # print("here", grad_input.shape)
+
+        return grad_input, None  # No gradient w.r.t. n
+
+
+# Testing gradcheck
+input_res = 10
+
+np_input = np.linspace(3, 5, input_res)
+
+np_input = np.reshape(np_input, newshape=(input_res, 1))
+
+input = torch.tensor(np_input, dtype=torch.float64, requires_grad=True)
+
+n = torch.tensor([0, 1, 2, 3, 4], dtype=torch.float64, requires_grad=False)
+
+input2 = torch.tensor(0.5, dtype=torch.float64, requires_grad=True)  # m
+
+
+thing = torch.autograd.gradcheck(torch_jn.apply, (input, n))
+print(thing)
+
+
+jn = torch_jn.apply
+
+
+def test_fun(z, n, m):
+    '''
+    Test composite function with 2 inputs (z and m) that require grad
+    '''
+    return jn(z, n) * m
+
+
+grad_test = test_fun(input, n, input2)
+
+print(grad_test)
+
+grad_output = torch.ones_like(grad_test)
+
+# If the output of your function is not a scalar, you need to explicitly
+# specify a gradient (also called the "initial gradient" or grad_output).
+# This is necessary because PyTorch needs to know how to propagate the gradient
+# back through the tensor's multiple output elements.
+
+# Here we provide a grad_output tensor, which will have the same shape as the
+# output of the torch_jn.apply function.
+# Looks like there is no need to call [input, input2] as done in main branch
+grad_test.backward(grad_output)
+
+print(input.grad)
+print(input2.grad)
