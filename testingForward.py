809995716430888a20e2d864e7bf73c29054dab9
orangeduice <osjack.oj@gmail.com>
2024-10-15 16:39:10 +0100

    Added file to test full forward equation. For some reason only works when in this location.

---
+++ testingForward.py
@@ -0,0 +1,59 @@
+import torch
+import numpy as np
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    import pymiediff
+
+    N_pt_test = 200
+    N_order_test = 1
+
+    n = torch.tensor(1)
+    wlRes = 10
+    wl = np.linspace(500,550, wlRes)
+    r_core = 37.4
+    r_shell = 150 # r_core + 100.0
+
+    n_env = 1
+    n_core = 4
+    n_shell = 0.1   + .7j
+
+    mu_env = 1
+    mu_core = 1
+    mu_shell = 1
+
+    dtype = torch.complex64
+    device = torch.device("cpu")
+
+    n_max = 5
+    k = 2 * np.pi / (wl / n_env)
+
+    m1 = n_core / n_env
+    m2 = n_shell / n_env
+    x = k * r_core
+    y = k * r_shell
+
+    x = torch.tensor(x, requires_grad=True, dtype=dtype)
+    y = torch.tensor(y, requires_grad=True, dtype=dtype)
+    m1 = torch.tensor(m1, requires_grad=True, dtype=dtype)
+    m2 = torch.tensor(m2, requires_grad=True, dtype=dtype)
+
+    print(x)
+
+    # a1 = psi(n, x)
+    a1 = pymiediff.an(x, y, n, m1, m2)
+    b1 = pymiediff.bn(x, y, n, m1, m2)
+
+    # a1 = pymiediff.psi(x, n)
+    # b1 = pymiediff.chi(x, n)
+
+
+    x_plot = x.detach().numpy().squeeze()
+    a1_plot = a1.detach().numpy().squeeze()
+    b1_plot = b1.detach().numpy().squeeze()
+
+    plt.plot(x_plot, a1_plot)
+    plt.plot(x_plot, b1_plot)
+    plt.show()
