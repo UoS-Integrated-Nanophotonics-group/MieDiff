08b909864f6f7c76fe74cb39e32656889c421d53
Oscar k c Jackson <osjack.oj@gmail.com>
2024-10-22 22:50:29 +0100

    Updated submodule testing for special with new plotters from helper.

--- pymiediff/special.py
+++ pymiediff/special.py
@@ -253,182 +253,54 @@ def xi_der(z, n):
 
 
 
-# %% ---- testing
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
+    import pymiediff
 
     N_pt_test = 50
-    N_order_test = 1
-
+    # n
     n = torch.tensor(5.0)
-    # z = torch.rand(3, dtype=torch.complex64).unsqueeze(0)
+
     # Jn
     z1 = torch.linspace(1, 20, N_pt_test) + 1j * torch.linspace(0.5, 3, N_pt_test)
     z1.requires_grad = True
-    # Yn
-    z2 = torch.linspace(0.5, 5, N_pt_test) + 1j * torch.linspace(1, 5, N_pt_test)
-    z2.requires_grad = True
     # dJn
-    z3 = torch.linspace(1, 20, N_pt_test) + 1j * torch.linspace(0.5, 3, N_pt_test)
+    z2 = torch.linspace(1, 20, N_pt_test) + 1j * torch.linspace(0.5, 3, N_pt_test)
+    z2.requires_grad = True
+    # Yn
+    z3 = torch.linspace(1, 2, N_pt_test) + 1j * torch.linspace(0.5, 3, N_pt_test)
     z3.requires_grad = True
     # dYn
-    z4 = torch.linspace(0.5, 5, N_pt_test) + 1j * torch.linspace(1, 5, N_pt_test)
+    z4 = torch.linspace(1, 2, N_pt_test) + 1j * torch.linspace(0.5, 3, N_pt_test)
     z4.requires_grad = True
 
-
-    result_Jn = Jn(n, z1)
-    result_Yn = Yn(n, z2)
-    result_dJn = dJn(n, z3)
-    result_dYn = dYn(n, z4)
-
-
-    # numerical center diff. for testing:
-    def num_dJn_dz(n, z, eps=0.0001 + 0.0001j):
-        z = z.conj()
-        fm = Jn(n, z - eps)
-        fp = Jn(n, z + eps)
-        dz = (fp - fm) / (2 * eps)
-        return dz
-
-    def num_dYn_dz(n, z, eps=0.0001 + 0.0001j):
-        z = z.conj()
-        fm = Yn(n, z - eps)
-        fp = Yn(n, z + eps)
-        dz = (fp - fm) / (2 * eps)
-        return dz
-
-    def num_ddJn_ddz(n, z, eps=0.0001 + 0.0001j):
-        z = z.conj()
-        fm = dJn(n, z - eps)
-        fp = dJn(n, z + eps)
-        dz = (fp - fm) / (2 * eps)
-        return dz
+    print(z1.dtype)
+    print(z2.dtype)
+    print(z3.dtype)
+    print(z4.dtype)
 
 
-    def num_ddYn_ddz(n, z, eps=0.0001 + 0.0001j):
-        z = z.conj()
-        fm = dYn(n, z - eps)
-        fp = dYn(n, z + eps)
-        dz = (fp - fm) / (2 * eps)
-        return dz
+    fig, ax = plt.subplots(4, 2, figsize=(16, 9), dpi=100, constrained_layout=True)
 
-    numdz_dJ = num_dJn_dz(n, z1)
-    numdz_dY = num_dYn_dz(n, z2)
-    numdz_ddJ = num_ddJn_ddz(n, z3)
-    numdz_ddY = num_ddYn_ddz(n, z4)
+    Jn_check = torch.autograd.gradcheck(Jn, (n, z1), eps=0.01)
+    dJn_check = torch.autograd.gradcheck(dJn, (n, z2), eps=0.01)
 
-    numdz_dJ_np = numdz_dJ.detach().numpy().squeeze()
-    numdz_dY_np = numdz_dY.detach().numpy().squeeze()
-    numdz_ddJ_np = numdz_ddJ.detach().numpy().squeeze()
-    numdz_ddY_np = numdz_ddY.detach().numpy().squeeze()
+    Yn_check = torch.autograd.gradcheck(Yn, (n, z3), eps=0.01)
+    dYn_check = torch.autograd.gradcheck(dYn, (n, z4), eps=0.01)
 
-    # batch eval does seem to work only for one dimension of outputs
-    # for (N,M) inputs, the sum of gradients along N seems to be calculated...
-    z_grad_Jn = torch.autograd.grad(
-        outputs=result_Jn,
-        inputs=[z1],
-        grad_outputs=torch.ones_like(result_Jn)
-    )
-    z_grad_Yn = torch.autograd.grad(
-        outputs=result_Yn,
-        inputs=[z2],
-        grad_outputs=torch.ones_like(result_Yn)
-    )
-    z_grad_dJn = torch.autograd.grad(
-        outputs=result_dJn,
-        inputs=[z3],
-        grad_outputs=torch.ones_like(result_dJn)
-    )
-    z_grad_dYn = torch.autograd.grad(
-        outputs=result_dYn,
-        inputs=[z4],
-        grad_outputs=torch.ones_like(result_dYn)
-    )
+    pymiediff.helper.FunctGradChecker(z1, Jn, (n, z1),
+                                      ax = (ax[0,0], ax[0,1]),
+                                      check = Jn_check)
+    pymiediff.helper.FunctGradChecker(z2, dJn, (n, z2),
+                                      ax = (ax[1,0], ax[1,1]),
+                                      check = dJn_check)
 
-    z_np = z1.detach().numpy().squeeze()
+    pymiediff.helper.FunctGradChecker(z3, Yn, (n, z3),
+                                      ax = (ax[2,0], ax[2,1]),
+                                      check = Yn_check)
+    pymiediff.helper.FunctGradChecker(z4, dYn, (n, z4),
+                                      ax = (ax[3,0], ax[3,1]),
+                                      check = dYn_check)
 
-    z_np_y = z2.detach().numpy().squeeze()
-
-    res_Jn_np = result_Jn.detach().numpy().squeeze()
-    res_Yn_np = result_Yn.detach().numpy().squeeze()
-    res_dJn_np = result_dJn.detach().numpy().squeeze()
-    res_dYn_np = result_dYn.detach().numpy().squeeze()
-
-    z_grad_plolt_Jn = z_grad_Jn[0].detach().numpy().squeeze()
-    z_grad_plolt_Yn = z_grad_Yn[0].detach().numpy().squeeze()
-    z_grad_plolt_dJn = z_grad_dJn[0].detach().numpy().squeeze()
-    z_grad_plolt_dYn = z_grad_dYn[0].detach().numpy().squeeze()
-
-
-    # Jn
-    plt.subplot(421, title="real Jn")
-    plt.plot(z_np, res_Jn_np.real, label="fwd")
-    plt.plot(z_np, numdz_dJ_np.real, label="num. grad.")
-    plt.plot(z_np, z_grad_plolt_Jn.real, label="AD grad.", dashes=[2, 2])
-    plt.legend()
-
-    plt.subplot(422, title="imag Jn")
-    plt.plot(z_np, res_Jn_np.imag, label="fwd")
-    plt.plot(z_np, numdz_dJ_np.imag, label="num. grad.")
-    plt.plot(z_np, z_grad_plolt_Jn.imag, label="AD grad.", dashes=[2, 2])
-    plt.legend()
-
-    # Yn
-
-    plt.subplot(423, title="real Yn")
-    plt.plot(z_np_y, res_Yn_np.real, label="fwd")
-    plt.plot(z_np_y, numdz_dY_np.real, label="num. grad.")
-    plt.plot(z_np_y, z_grad_plolt_Yn.real, label="AD grad.", dashes=[2, 2])
-    plt.legend()
-
-    plt.subplot(424, title="imag Yn")
-    plt.plot(z_np_y, res_Yn_np.imag, label="fwd")
-    plt.plot(z_np_y, numdz_dY_np.imag, label="num. grad.")
-    plt.plot(z_np_y, z_grad_plolt_Yn.imag, label="AD grad.", dashes=[2, 2])
-    plt.legend()
-
-    # dJn
-
-    plt.subplot(425, title="real dJn")
-    plt.plot(z_np, res_dJn_np.real, label="fwd")
-    plt.plot(z_np, numdz_ddJ_np.real, label="num. grad.")
-    plt.plot(z_np, z_grad_plolt_dJn.real, label="AD grad.", dashes=[2, 2])
-    plt.legend()
-
-    plt.subplot(426, title="imag dJn")
-    plt.plot(z_np, res_dJn_np.imag, label="fwd")
-    plt.plot(z_np, numdz_ddJ_np.imag, label="num. grad.")
-    plt.plot(z_np, z_grad_plolt_dJn.imag, label="AD grad.", dashes=[2, 2])
-    plt.legend()
-
-    # dYn
-
-    plt.subplot(427, title="real dYn")
-    plt.plot(z_np_y, res_dYn_np.real, label="fwd")
-    plt.plot(z_np_y, numdz_ddY_np.real, label="num. grad.")
-    plt.plot(z_np_y, z_grad_plolt_dYn.real, label="AD grad.", dashes=[2, 2])
-    plt.legend()
-
-    plt.subplot(428, title="imag dYn")
-    plt.plot(z_np_y, res_dYn_np.imag, label="fwd")
-    plt.plot(z_np_y, numdz_ddY_np.imag, label="num. grad.")
-    plt.plot(z_np_y, z_grad_plolt_dYn.imag, label="AD grad.", dashes=[2, 2])
-    plt.legend()
-
-
-
-
-    plt.tight_layout()
     plt.show()
-
-    # caution with complex arguments:
-    # pytorch uses internally the convention that complex derivatives are wrt the conjugate:
-    # derivative = dF/dz*.
-    # Therefore, there is a sign difference at the imag part of the output, which needs
-    # to be accounted for in the backward pass implementation.
-    # see: https://pytorch.org/docs/stable/notes/autograd.html#autograd-for-complex-numbers
-
-    torch.autograd.gradcheck(Jn, (n, z1), eps=0.01)
-    torch.autograd.gradcheck(Yn, (n, z2), eps=0.01)
-    torch.autograd.gradcheck(dJn, (n, z3), eps=0.01)
-    torch.autograd.gradcheck(dYn, (n, z4), eps=0.01)
