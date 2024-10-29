7db80d80759ae0d185dde2740f687466732faf05
orangeduice <osjack.oj@gmail.com>
2024-10-29 13:59:43 +0000

    Moved scattering equation to submodule.

--- forwardAndBackwardTesting.py
+++ forwardAndBackwardTesting.py
@@ -54,14 +54,9 @@ if __name__ == "__main__":
     b3 = pymiediff.coreshell.bn(x, y, n3, m1, m2)
     b4 = pymiediff.coreshell.bn(x, y, n4, m1, m2)
 
-    def cross_sca(k, n1, a1, b1, n2, a2, b2, n3, a3, b3, n4, a4, b4):
-        return ((2*torch.pi/k**2) *
-                ((2*n1 + 1)*(a1.abs()**2 + b1.abs()**2) +
-                 (2*n2 + 1)*(a2.abs()**2 + b2.abs()**2) +
-                 (2*n3 + 1)*(a3.abs()**2 + b3.abs()**2) +
-                 (2*n4 + 1)*(a4.abs()**2 + b4.abs()**2)))
-
-    Csca = cross_sca(k, n1, a1, b1, n2, a2, b2, n3, a3, b3, n4, a4, b4)
+    Csca = pymiediff.coreshell.cross_sca(
+        k, n1, a1, b1, n2, a2, b2, n3, a3, b3, n4, a4, b4
+    )
     Csca_np = Csca.detach().numpy()
 
     checkAutograd = True
@@ -69,18 +64,15 @@ if __name__ == "__main__":
     checkGrad = False
 
     if checkAutograd:
-        check = torch.autograd.gradcheck(cross_sca, [k,
-                                                     n1, a1, b1,
-                                                     n2, a2, b2,
-                                                     n3, a3, b3,
-                                                     n4, a4, b4],
-                                         eps=0.01)
+        check = torch.autograd.gradcheck(
+            pymiediff.coreshell.cross_sca, [k, n1, a1, b1, n2, a2, b2, n3, a3, b3, n4, a4, b4], eps=0.01
+        )
         print("autograd.gradcheck positive?", check)
 
     if checkGrad:
-        r_c_grad = torch.autograd.grad(outputs=Csca,
-                                       inputs=[r_c, r_s, m1, m2],
-                                       grad_outputs=torch.ones_like(Csca))
+        r_c_grad = torch.autograd.grad(
+            outputs=Csca, inputs=[r_c, r_s, m1, m2], grad_outputs=torch.ones_like(Csca)
+        )
         print(r_c_grad)
 
     if checkForward:
@@ -89,33 +81,35 @@ if __name__ == "__main__":
 
             CscaReal = []
             for wavelengh in wl:
-                scatter = ps.MieQCoreShell(mCore=n_core,
-                                        mShell=n_shell,
-                                        wavelength=wavelengh,
-                                        dCore=2 * r_core,
-                                        dShell=2 * r_shell,
-                                        nMedium=n_env,
-                                        asCrossSection=False,
-                                        asDict=True)["Qsca"]
+                scatter = ps.MieQCoreShell(
+                    mCore=n_core,
+                    mShell=n_shell,
+                    wavelength=wavelengh,
+                    dCore=2 * r_core,
+                    dShell=2 * r_shell,
+                    nMedium=n_env,
+                    asCrossSection=False,
+                    asDict=True,
+                )["Qsca"]
 
                 CscaReal.append(scatter)
             plt.plot(CscaReal, ls="--")
 
         fig, ax = plt.subplots(figsize=(10, 5), dpi=200)
 
+        multipoles = pymiediff.helper.MakeMultipoles(
+            [a1, a2, a3, a4], [b1, b2, b3, b4], k
+        )
 
-        multipoles = pymiediff.helper.MakeMultipoles([a1, a2, a3, a4],
-                                                     [b1, b2, b3, b4],
-                                                     k)
-
-        pymiediff.helper.PlotScatteringCrossSection(ax,
-                                                    (r_core,
-                                                     r_shell),
-                                                    (m1.detach().numpy().item(),
-                                                     m2.detach().numpy().item()),
-                                                    wl, Csca_np.real,
-                                                    max_dis = 3,
-                                                    multipoles = multipoles,
-                                                    norm=np.pi*(r_shell)**2)
+        pymiediff.helper.PlotScatteringCrossSection(
+            ax,
+            (r_core, r_shell),
+            (m1.detach().numpy().item(), m2.detach().numpy().item()),
+            wl,
+            Csca_np.real,
+            max_dis=3,
+            multipoles=multipoles,
+            norm=np.pi * (r_shell) ** 2,
+        )
 
         plt.show()
