42aaba18830a309f9822f4e4b55e6c7d0d48c80e
Oscar k c Jackson <osjack.oj@gmail.com>
2024-11-12 15:43:52 +0000

    Added contraint to shell raduis to keep it physical. Improved optimiser example, now only optimises radii. New loss calculation. Credit to Peter.

--- testing/particle_optimiser_test.py
+++ testing/particle_optimiser_test.py
@@ -1,8 +1,10 @@
+# %%
 import torch
 import numpy as np
 import time
 import torch.nn.functional as F
 
+# %%
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
@@ -11,7 +13,7 @@ if __name__ == "__main__":
     it = []
     losses = []
 
-    N_pt_test = 200
+    N_pt_test = 50
     N_order_test = 1
 
     n1 = torch.tensor(1)
@@ -22,13 +24,13 @@ if __name__ == "__main__":
     n6 = torch.tensor(4)
 
     wlRes = 100
-    wl = np.linspace(200, 600, wlRes)
-    r_core = 12.0
-    r_shell = 50.0
+    wl = np.linspace(400, 800, wlRes)
+    r_core = 80.0
+    r_shell = r_core + 10.0
 
     n_env = 1
-    n_core = 2 + 0j
-    n_shell = 5 + 0j
+    n_core = 3 + 0j
+    n_shell = 2.0
 
     dtype = torch.complex128  # torch.complex64
     device = torch.device("cpu")
@@ -45,8 +47,8 @@ if __name__ == "__main__":
     x = k * r_c
     y = k * r_s
 
-    m1 = torch.tensor(m1, requires_grad=True, dtype=dtype)
-    m2 = torch.tensor(m2, requires_grad=True, dtype=dtype)
+    m1 = torch.tensor(m1, requires_grad=False, dtype=dtype)
+    m2 = torch.tensor(m2, requires_grad=False, dtype=dtype)
     t0 = time.time()
     a1 = pymiediff.coreshell.an(x, y, n1, m1, m2)
     a2 = pymiediff.coreshell.an(x, y, n2, m1, m2)
@@ -62,6 +64,8 @@ if __name__ == "__main__":
         k, n1, a1, b1, n2, a2, b2, n3, a3, b3, n4, a4, b4
     )
 
+
+
     import PyMieScatt as ps
 
     y_real = []
@@ -70,8 +74,8 @@ if __name__ == "__main__":
             mCore=n_core,
             mShell=n_shell,
             wavelength=wavelengh,
-            dCore=1.99 * r_core,
-            dShell=1.99 * r_shell,
+            dCore=2.1 * r_core,
+            dShell=2.1 * r_shell,
             nMedium=n_env,
             asCrossSection=False,
             asDict=True,
@@ -81,12 +85,33 @@ if __name__ == "__main__":
 
     CscaTarget = torch.tensor(y_real)
 
-    optimizer = torch.optim.SGD([r_c, r_s, m1, m2], lr=0.00000001)
+    y_start = (
+        pymiediff.coreshell.cross_sca(k, n1, a1, b1, n2, a2, b2, n3, a3, b3, n4, a4, b4)
+        .detach()
+        .numpy()
+    )
 
-    for i in range(100):
+    # plt.plot(wl, y_real, label="target")
+    # plt.plot(
+    #    wl, y_start / (np.pi * (r_shell) ** 2), label="pymiediff-start.", dashes=[2, 2]
+    # )
+    plt.legend()
+    # %%
+    optimizer = torch.optim.SGD([r_c, r_s, m1, m2], lr=0.01)
+    optimizer = torch.optim.Adam([r_c, r_s], lr=0.3)
+    norm = 1.0
+
+    for i in range(200):
+
+        if r_s < r_c:
+            size_constraint = (r_s - r_c) * 10
+            r_s_constrained = r_c.item()
+        else:
+            r_s_constrained = r_s
+            size_constraint = 0
 
         x = k * r_c
-        y = k * r_s
+        y = k * r_s_constrained
 
         a1 = pymiediff.coreshell.an(x, y, n1, m1, m2)
         a2 = pymiediff.coreshell.an(x, y, n2, m1, m2)
@@ -100,14 +125,15 @@ if __name__ == "__main__":
 
         optimizer.zero_grad()
 
-        # print("here", r_c)
-
-        guess = pymiediff.coreshell.cross_sca(
+        guess_scs = pymiediff.coreshell.cross_sca(
             k, n1, a1, b1, n2, a2, b2, n3, a3, b3, n4, a4, b4
         )
-        loss = F.mse_loss(guess.real, CscaTarget.real)
+        guess_qsca = guess_scs / (torch.pi * (r_shell) ** 2)
+        loss = torch.mean(torch.abs((guess_qsca.real / norm - CscaTarget.real / norm)) ** 2)
+        loss = loss - size_constraint
 
-        loss.backward(retain_graph=True, create_graph=True)
+        loss.backward()
+        # loss.backward(retain_graph=True, create_graph=True)
 
         optimizer.step()
 
@@ -115,18 +141,28 @@ if __name__ == "__main__":
             it.append(i)
             losses.append(loss.item())
             print(
-                "Step {}: r_c, r_s, m1, m2 = {}, {}, {}, {} loss = {}".format(
-                    i + 1, r_c.item(), r_s.item(), m1.item(), m2.item(), loss.item()
+                "Step {}: r_c, r_s, m1, m2 = {}, {}, {}, {} loss = {}, size_constraint={}".format(
+                    i + 1,
+                    r_c.item(),
+                    r_s.item(),
+                    m1.item(),
+                    m2.item(),
+                    loss.item(),
+                    size_constraint,
                 )
             )
 
+    y_opt = (
+        pymiediff.coreshell.cross_sca(k, n1, a1, b1, n2, a2, b2, n3, a3, b3, n4, a4, b4)
+        .detach()
+        .numpy()
+    )
 
-    y_opt = pymiediff.coreshell.cross_sca(
-            k, n1, a1, b1, n2, a2, b2, n3, a3, b3, n4, a4, b4
-        ).detach().numpy()
-
-    plt.plot(wl, y_real, label="Real")
-    plt.plot(wl, y_opt/(np.pi * (r_shell) ** 2), label="Opt.", dashes=[2, 2])
+    plt.plot(wl, y_real, label="target  r_c, r_s, m1, m2 = {}, {}, {}, {}".format(r_core , r_shell, n_core / n_env, n_shell / n_env))
+    plt.plot(
+        wl, y_start / (np.pi * (r_shell) ** 2), label="pymiediff-start. r_c, r_s, m1, m2 = {}, {}, {}, {}".format(1.05 * r_core, 1.05 * r_shell, n_core / n_env, n_shell / n_env), dashes=[2, 2]
+    )
+    plt.plot(wl, y_opt / (np.pi * (r_shell) ** 2), label="Opt. r_c, r_s, m1, m2 = {}, {}, {}, {}".format(r_c.item(), r_s.item(), m1.item(), m2.item()), dashes=[2, 2])
     plt.legend()
 
     plt.tight_layout()
