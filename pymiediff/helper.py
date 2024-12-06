d145e5eac490b35ef19cd3e4851de30c3ef7bf06
Oscar k c Jackson <osjack.oj@gmail.com>
2024-12-06 01:18:00 +0000

    Reformated code and modifed for new multipole method.

--- pymiediff/helper.py
+++ pymiediff/helper.py
@@ -2,44 +2,123 @@ import numpy as np
 import torch
 
 
-def PlotScatteringCrossSection(ax, radi, m,  waveLengths, scattering, max_dis = 2, multipoles = None, norm = 1, sizeParameter = False, prefix = "nm"):
-    keys = [["ED",(0, (3, 5, 1, 5, 1, 5)),"firebrick" ,"MD",(0, (3, 1, 1, 1, 1, 1)),"royalblue" ],
-            ["EQ",(0, (3, 5, 1, 5)),"maroon" ,"MQ",(0, (3, 1, 1, 1)), "navy"],
-            ["EO",(0, (5, 1)),"indianred" ,"MO",(0, (5, 5)),"cornflowerblue"],
-            ["E16",(0, (1, 1)),"lightcoral" ,"M16" ,(0, (1, 5)),"slateblue"]]
+keys = [
+    [
+        "ED",
+        (0, (3, 5, 1, 5, 1, 5)),
+        "firebrick",
+        "MD",
+        (0, (3, 1, 1, 1, 1, 1)),
+        "royalblue",
+    ],
+    ["EQ", (0, (3, 5, 1, 5)), "maroon", "MQ", (0, (3, 1, 1, 1)), "navy"],
+    ["EO", (0, (5, 1)), "indianred", "MO", (0, (5, 5)), "cornflowerblue"],
+    ["E16", (0, (1, 1)), "lightcoral", "M16", (0, (1, 5)), "slateblue"],
+]
 
-    if sizeParameter:
-        var = "Size Parameter $ x=k a=\\frac{2 \pi n a}{\lambda}$"
+
+def detachFun(args, item=False):
+    # If args is a tuple, process its elements; otherwise, process the single tensor
+    if isinstance(args, tuple) and not item:
+        return tuple(x.detach().numpy() for x in args)
+    elif isinstance(args, tuple) and item:
+        return tuple(x.detach().numpy().item() for x in args)
     else:
-        var = "$\lambda$"
+        return args.detach().numpy()
 
+
+def PlotCrossSection(
+    ax,
+    radi,
+    ns,
+    waveLengths,
+    scattering,
+    names=["total"],
+    max_dis=2,
+    multipoles=None,
+    norm=1,
+    prefix="nm",
+    title=None,
+):
+
+    radi = detachFun(radi, item=True)
+    ns = detachFun(ns, item=True)
+    waveLengths = waveLengths.detach().numpy()
+    scattering = detachFun(scattering)
+
+    var = "$\lambda$ ({})".format(prefix)
     if multipoles is not None:
-        n_num = len(multipoles)
-        for n in range(1,n_num+1):
+        n_num = multipoles.shape[-1]
+        for n in range(1, n_num + 1):
             if (n <= max_dis) and (n <= 4):
-                ax.plot(waveLengths, multipoles[n-1][0]/norm, label = keys[n-1][0], linestyle = keys[n-1][1], color=keys[n-1][2], linewidth=0.8)
-                ax.plot(waveLengths, multipoles[n-1][1]/norm, label = keys[n-1][3], linestyle = keys[n-1][4], color=keys[n-1][5], linewidth=0.8)
-            elif (n <= max_dis):
-                ax.plot(waveLengths, multipoles[n-1][0]/norm, color="red", linewidth=0.8)
-                ax.plot(waveLengths, multipoles[n-1][1]/norm, color="blue", linewidth=0.8)
+                ax.plot(
+                    waveLengths,
+                    multipoles[0, :, n - 1] / norm,
+                    label=keys[n - 1][0],
+                    linestyle=keys[n - 1][1],
+                    color=keys[n - 1][2],
+                    linewidth=0.8,
+                )
+                ax.plot(
+                    waveLengths,
+                    multipoles[1, :, n - 1] / norm,
+                    label=keys[n - 1][3],
+                    linestyle=keys[n - 1][4],
+                    color=keys[n - 1][5],
+                    linewidth=0.8,
+                )
+            elif n <= max_dis:
+                ax.plot(
+                    waveLengths,
+                    multipoles[0, :, n - 1] / norm,
+                    color="red",
+                    linewidth=0.8,
+                )
+                ax.plot(
+                    waveLengths,
+                    multipoles[1, :, n - 1] / norm,
+                    color="blue",
+                    linewidth=0.8,
+                )
+    if isinstance(scattering, tuple):
+        for i, name in zip(scattering, names):
+            ax.plot(waveLengths, i / norm, label=name, linewidth=0.8)
+    else:
+        ax.plot(waveLengths, scattering / norm, label=names, linewidth=0.8)
 
-    ax.plot(waveLengths, scattering/norm, c = "black", label = "Total",linewidth=0.8)
     ax.set_xlabel(var)
     if norm != 1:
         ax.set_ylabel("$\sigma_{s}/r_{shell}^2$ ")
     else:
         ax.set_ylabel("$\sigma_{s}$ ($m^{2}$)")
-    ax.set_title("Scattering cross section of shell sphere of radi, $r_i = {}$ nm, and corresponiding relative reflection index $m_i = {}$".format(str(radi)[1:-1], str(m)[1:-1]), fontsize=10)
-    #ax.set_title("Scattering cross section of sphere of raduis, a = " + str(a) + prefix + " and $m=n_{1}/n=$" + str(m) + "\n calculated from Mie-Theroy displaying up to n = " + str(max_dis))
-    #ax.set_title("Scattering cross section of a sphere showing the different scattering regimes")
+    if title is None:
+        ax.set_title(
+            "Scattering cross section of shell sphere of radi, $r_i = {}$ nm, and corresponiding reflection index $n_i = {}$".format(
+                str(radi)[1:-1], str(ns)[1:-1]
+            ),
+            fontsize=10,
+        )
+    else:
+        ax.set_title(
+            title,
+            fontsize=10,
+        )
     ax.legend()
 
 
 def MakeMultipoles(As, Bs, k):
     multipoles = []
-    for n, (a, b) in enumerate(zip(As,Bs)):
-        multipoles.append([(2*np.pi/k.detach().numpy()**2) * (2*(n + 1) + 1)* (a.abs()**2).detach().numpy(),
-                           (2*np.pi/k.detach().numpy()**2) * (2*(n + 1) + 1)* (b.abs()**2).detach().numpy() ])
+    for n, (a, b) in enumerate(zip(As, Bs)):
+        multipoles.append(
+            [
+                (2 * np.pi / k.detach().numpy() ** 2)
+                * (2 * (n + 1) + 1)
+                * (a.abs() ** 2).detach().numpy(),
+                (2 * np.pi / k.detach().numpy() ** 2)
+                * (2 * (n + 1) + 1)
+                * (b.abs() ** 2).detach().numpy(),
+            ]
+        )
     return multipoles
 
 
@@ -52,7 +131,7 @@ def NumCenterDiff(Funct, n, z, eps=0.0001 + 0.0001j):
     return dz
 
 
-def GradCheckerPlot(ax1, ax2, z, fwd, grad, num_grad, name, check = None):
+def GradCheckerPlot(ax1, ax2, z, fwd, grad, num_grad, name, check=None):
     ax1.set_title("Real {}. Passed grad check: {}".format(name, check))
     ax1.plot(z, fwd.real, label="Forward")
     ax1.plot(z, num_grad.real, label="Num. grad.")
@@ -68,12 +147,12 @@ def GradCheckerPlot(ax1, ax2, z, fwd, grad, num_grad, name, check = None):
     ax2.legend()
 
 
-def FunctGradChecker(z, funct, inputs, ax = None, check = None):
+def FunctGradChecker(z, funct, inputs, ax=None, check=None):
     result = funct(*inputs)
     num_grad = NumCenterDiff(funct, *inputs)
-    grad = torch.autograd.grad(outputs=result,
-                               inputs=[z],
-                               grad_outputs=torch.ones_like(result))
+    grad = torch.autograd.grad(
+        outputs=result, inputs=[z], grad_outputs=torch.ones_like(result)
+    )
 
     result_np = result.detach().numpy().squeeze()
     z_np = z.detach().numpy().squeeze()
@@ -81,24 +160,15 @@ def FunctGradChecker(z, funct, inputs, ax = None, check = None):
     grad_np = grad[0].detach().numpy().squeeze()
 
     if ax is not None:
-        GradCheckerPlot(ax[0],
-                        ax[1],
-                        z_np,
-                        result_np,
-                        grad_np,
-                        num_grad_np,
-                        funct.__name__,
-                        check = check)
+        GradCheckerPlot(
+            ax[0],
+            ax[1],
+            z_np,
+            result_np,
+            grad_np,
+            num_grad_np,
+            funct.__name__,
+            check=check,
+        )
 
     return z_np, num_grad_np, grad_np
-
-
-
-
-
-
-
-
-
-
-
