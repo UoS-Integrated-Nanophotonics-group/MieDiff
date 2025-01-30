cb6618890f1a707619819cb058e8ec38495d0f56
Oscar k c Jackson <osjack.oj@gmail.com>
2025-01-30 18:33:24 +0000

    Removed now reduntant code, and moved sample plots to farfield submodule.

--- pymiediff/farfield.py
+++ pymiediff/farfield.py
@@ -200,3 +200,171 @@ def angular_scattering(k0, theta, r_c, eps_c, an = coreshell.an, bn = coreshell.
         i_unp=i_unp,
         P=P,
     )
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import time
+    import pymiediff as pmd
+
+    # ======== Test Parameters ==========
+    N_pt_test = 1000
+    # N_order_test = 6
+
+    # Test Case for testing:
+    core_radius = 12.0  # nm
+    shell_radius = 50.0  # nm
+    core_refractiveIndex = 2.0 + 0.1j
+    shell_refractiveIndex = 5.0 + 0.2j
+    starting_wavelength = 200.0  # nm
+    ending_wavelength = 600.0  # nm
+    # Wavelegth for anular
+    target_wavelength = 500.0 # nm
+    # ===================================
+
+    dtype = torch.complex128  # torch.complex64
+    device = torch.device("cpu")
+
+    k0 = (
+        2 * torch.pi / torch.linspace(starting_wavelength, ending_wavelength, N_pt_test)
+    )
+
+    k0_single = torch.tensor(2 * torch.pi / target_wavelength)
+
+    theta = torch.linspace(0.001, 2 * torch.pi - 0.001, N_pt_test, dtype=torch.double)
+    r_c = torch.tensor(core_radius)
+    r_s = torch.tensor(shell_radius)
+    n_c = torch.tensor(core_refractiveIndex)
+    n_s = torch.tensor(shell_refractiveIndex)
+
+    t0 = time.time()
+
+    cross_section = cross_sections(
+        k0=k0,
+        r_c=r_c,
+        eps_c=n_c**2,
+        r_s=r_s,
+        eps_s=n_s**2,
+        eps_env=1,
+    )
+
+    t1 = time.time()
+    time_torch = t1 - t0
+    print("Time taken:", time_torch)
+
+    cross_section_sca = cross_section["q_sca"]
+    cross_section_ext = cross_section["q_ext"]
+    cross_section_abs = cross_section["q_abs"]
+    cross_section_geo = cross_section["cs_geo"]
+
+    multipoles = cross_sections_mp(
+        k0=k0,
+        r_c=r_c,
+        eps_c=n_c**2,
+        r_s=r_s,
+        eps_s=n_s**2,
+        eps_env=1,
+    )
+
+    multipole_sca = multipoles["q_sca"]
+
+    ang_scattering = angular_scattering(
+        k0=k0_single,
+        theta=theta,
+        r_c=r_c,
+        eps_c=n_c**2,
+        r_s=r_s,
+        eps_s=n_s**2,
+        eps_env=1,
+    )
+
+    s1 = ang_scattering["S1"]
+    s2 = ang_scattering["S2"]
+    i_per = ang_scattering["i_per"]
+    i_par = ang_scattering["i_par"]
+    i_unp = ang_scattering["i_unp"]
+    P = ang_scattering["P"]
+
+
+    # Plotting
+    fig = plt.figure()
+    # fig, ax = plt.subplots(2, 2, figsize=(12, 5), dpi=200)
+
+    ax1 = fig.add_subplot(221)
+    pmd.helper.PlotCrossSection(
+        ax1,
+        radi=(r_c, r_s),
+        ns=(n_c, n_s),
+        waveLengths=(2 * np.pi) / k0,
+        scattering=(cross_section_sca, cross_section_ext, cross_section_abs),
+        names=("sca", "ext", "abs"),
+    )
+
+    ax2 = fig.add_subplot(223)
+    pmd.helper.PlotCrossSection(
+        ax2,
+        radi=(r_c, r_s),
+        ns=(n_c, n_s),
+        waveLengths=(2 * np.pi) / k0,
+        scattering=cross_section_sca,
+        names="sca",
+        multipoles=multipole_sca,
+        max_dis=4,
+        title="Multipole decomp.",
+    )
+
+    ax3 = fig.add_subplot(222, projection='polar')
+    pmd.helper.PlotAngular(
+        ax3,
+        radi=(r_c, r_s),
+        ns=(n_c, n_s),
+        wavelength=(2 * np.pi) / k0_single,
+        angles=theta,
+        scattering=(i_per, i_par, i_unp),
+        names=("$i_{per}$", "$i_{par}$", "$i_{unp}$"),
+        title=f"Scattered irradiance per unit incident irradiance at $\lambda = {target_wavelength}$."
+    )
+
+    ax4 = fig.add_subplot(224, projection='polar')
+    pmd.helper.PlotAngular(
+        ax4,
+        radi=(r_c, r_s),
+        ns=(n_c, n_s),
+        wavelength=(2 * np.pi) / k0_single,
+        angles=theta,
+        scattering=(s1, s2),
+        names=("$S_1$", "$S_2$"),
+        title="Corresponding S parameters."
+    )
+
+    fig.tight_layout()
+    plt.show()
+
+    # import PyMieScatt as pms
+
+    # wl = np.linspace(starting_wavelength, ending_wavelength, N_pt_test)
+
+    # cexts = []
+    # cscas = []
+    # cabss = []
+
+    # for w in wl:
+    #     cext, csca, cabs, _, _, _, _ = pms.MieQCoreShell(
+    #         core_refractiveIndex,
+    #         shell_refractiveIndex,
+    #         w,
+    #         core_radius * 2,
+    #         shell_radius * 2,
+    #     )
+
+    #     cexts.append(cext)
+    #     cscas.append(csca)
+    #     cabss.append(cabs)
+
+    # ax[0].plot(wl, cexts, label="pms cext", linestyle="--", linewidth=1)
+    # ax[0].plot(wl, cscas, label="pms csca", linestyle="--", linewidth=1)
+    # ax[0].plot(wl, cabss, label="pms cabs", linestyle="--", linewidth=1)
+    # ax[0].legend()
+
+    # plt.show()
+
