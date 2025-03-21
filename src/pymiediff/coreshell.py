e8299ec917d4d8e69ffb67d2500a58a37af12761
Oscar Jackson <osjack.oj@gmail.com>
2025-03-21 16:13:44 +0000

    added: cn and dn coefficients

--- src/pymiediff/coreshell.py
+++ src/pymiediff/coreshell.py
@@ -4,12 +4,10 @@ Core-shell scattering coefficients
 
 analytical solutions taken from
 
-Bohren, Craig F., and Donald R. Huffman. 
+Bohren, Craig F., and Donald R. Huffman.
 Absorption and scattering of light by small particles. John Wiley & Sons, 2008.
 """
 from pymiediff.special import psi, psi_der, chi, chi_der, xi, xi_der
-
-
 
 
 def _An(x, n, m1, m2):
@@ -17,7 +15,7 @@ def _An(x, n, m1, m2):
 
     Absorption and scattering of light by small particles.
     Pg. and Equ. number.
-    
+
     Args:
         x (torch.Tensor): size parameter (core)
         n (torch.Tensor): orders to compute
@@ -41,7 +39,7 @@ def _Bn(x, n, m1, m2):
 
     Absorption and scattering of light by small particles.
     Pg. and Equ. number.
-    
+
     Args:
         x (torch.Tensor): size parameter (core)
         n (torch.Tensor): orders to compute
@@ -62,7 +60,7 @@ def _Bn(x, n, m1, m2):
 
 def an(x, y, n, m1, m2):
     """an scattering coefficient
- 
+
     Absorption and scattering of light by small particles.
     Pg. and Equ. number.
 
@@ -107,4 +105,64 @@ def bn(x, y, n, m1, m2):
     ) / (
         m2 * xi(y, n) * (psi_der(m2 * y, n) - _Bn(x, n, m1, m2) * chi_der(m2 * y, n))
         - xi_der(y, n) * (psi(m2 * y, n) - _Bn(x, n, m1, m2) * chi(m2 * y, n))
+    )
+
+
+def cn(x, y, n, m1, m2):
+    """cn scattering coefficient
+
+    Args:
+        x (torch.Tensor): size parameter (core)
+        y (torch.Tensor): size parameter (shell)
+        n (torch.Tensor): orders to compute
+        m1 (torch.Tensor): relative refractive index of core
+        m2 (torch.Tensor): relative refractive index of shell
+
+    Returns:
+       torch.Tensor: result
+    """
+    return (
+        chi_der(m2 * x, n) * m1 * m2 * psi_der(y, n) * psi(m2 * x, n) * xi(y, n)
+        - chi_der(m2 * x, n) * m1 * m2 * psi(m2 * x, n) * psi(y, n) * xi_der(y, n)
+        - chi(m2 * x, n) * m1 * m2 * psi_der(m2 * x, n) * psi_der(y, n) * xi(y, n)
+        + chi(m2 * x, n) * m1 * m2 * psi_der(m2 * x, n) * psi(y, n) * xi_der(y, n)
+    ) / (
+        chi_der(m2 * x, n) * m2**2 * psi_der(m2 * y, n) * psi(m1 * x, n) * xi(y, n)
+        - chi_der(m2 * x, n) * m2 * psi(m1 * x, n) * psi(m2 * y, n) * xi_der(y, n)
+        + chi_der(m2 * y, n) * m1 * m2 * psi_der(m1 * x, n) * psi(m2 * x, n) * xi(y, n)
+        - chi_der(m2 * y, n) * m2**2 * psi_der(m2 * x, n) * psi(m1 * x, n) * xi(y, n)
+        - chi(m2 * x, n) * m1 * m2 * psi_der(m1 * x, n) * psi_der(m2 * y, n) * xi(y, n)
+        + chi(m2 * x, n) * m1 * psi_der(m1 * x, n) * psi(m2 * y, n) * xi_der(y, n)
+        - chi(m2 * y, n) * m1 * psi_der(m1 * x, n) * psi(m2 * x, n) * xi_der(y, n)
+        + chi(m2 * y, n) * m2 * psi_der(m2 * x, n) * psi(m1 * x, n) * xi_der(y, n)
+    )
+
+
+def dn(x, y, n, m1, m2):
+    """dn scattering coefficient
+
+    Args:
+        x (torch.Tensor): size parameter (core)
+        y (torch.Tensor): size parameter (shell)
+        n (torch.Tensor): orders to compute
+        m1 (torch.Tensor): relative refractive index of core
+        m2 (torch.Tensor): relative refractive index of shell
+
+    Returns:
+       torch.Tensor: result
+    """
+    return (
+        -chi_der(m2 * x, n) * m1 * m2 * psi_der(y, n) * psi(m2 * x, n) * xi(y, n)
+        + chi_der(m2 * x, n) * m1 * m2 * psi(m2 * x, n) * psi(y, n) * xi_der(y, n)
+        + chi(m2 * x, n) * m1 * m2 * psi_der(m2 * x, n) * psi_der(y, n) * xi(y, n)
+        - chi(m2 * x, n) * m1 * m2 * psi_der(m2 * x, n) * psi(y, n) * xi_der(y, n)
+    ) / (
+        chi_der(m2 * x, n) * m1 * m2 * psi(m1 * x, n) * psi(m2 * y, n) * xi_der(y, n)
+        - chi_der(m2 * x, n) * m1 * psi_der(m2 * y, n) * psi(m1 * x, n) * xi(y, n)
+        + chi_der(m2 * y, n) * m1 * psi_der(m2 * x, n) * psi(m1 * x, n) * xi(y, n)
+        - chi_der(m2 * y, n) * m2 * psi_der(m1 * x, n) * psi(m2 * x, n) * xi(y, n)
+        - chi(m2 * x, n) * m2**2 * psi_der(m1 * x, n) * psi(m2 * y, n) * xi_der(y, n)
+        + chi(m2 * x, n) * m2 * psi_der(m1 * x, n) * psi_der(m2 * y, n) * xi(y, n)
+        - chi(m2 * y, n) * m1 * m2 * psi_der(m2 * x, n) * psi(m1 * x, n) * xi_der(y, n)
+        + chi(m2 * y, n) * m2**2 * psi_der(m1 * x, n) * psi(m2 * x, n) * xi_der(y, n)
     )
