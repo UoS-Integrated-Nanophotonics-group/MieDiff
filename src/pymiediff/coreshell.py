db23c1fc0bf143b2dc8be2a2920e91a7751a816f
Oscar Jackson <osjack.oj@gmail.com>
2025-03-21 01:33:58 +0000

    doc: added documentation and doc strings for coreshell module

--- src/pymiediff/coreshell.py
+++ src/pymiediff/coreshell.py
@@ -1,7 +1,30 @@
 from pymiediff.special import psi, psi_der, chi, chi_der, xi, xi_der
 
+"""
+Core-shell scattering coefficients
+
+analytical solutions taken from
+
+Bohren, Craig F., and Donald R. Huffman. 
+Absorption and scattering of light by small particles. John Wiley & Sons, 2008.
+"""
+
 
 def _An(x, n, m1, m2):
+    """private An scattering coefficient
+
+    Absorption and scattering of light by small particles.
+    Pg. and Equ. number.
+    
+    Args:
+        x (torch.Tensor): size parameter (core)
+        n (torch.Tensor): orders to compute
+        m1 (torch.Tensor): relative refractive index of core
+        m2 (torch.Tensor): relative refractive index of shell
+
+    Returns:
+        torch.Tensor: result
+    """
     return (
         m2 * psi(m2 * x, n) * psi_der(m1 * x, n)
         - m1 * psi_der(m2 * x, n) * psi(m1 * x, n)
@@ -12,6 +35,20 @@ def _An(x, n, m1, m2):
 
 
 def _Bn(x, n, m1, m2):
+    """private Bn scattering coefficient
+
+    Absorption and scattering of light by small particles.
+    Pg. and Equ. number.
+    
+    Args:
+        x (torch.Tensor): size parameter (core)
+        n (torch.Tensor): orders to compute
+        m1 (torch.Tensor): relative refractive index of core
+        m2 (torch.Tensor): relative refractive index of shell
+
+    Returns:
+        torch.Tensor: result
+    """
     return (
         m2 * psi(m1 * x, n) * psi_der(m2 * x, n)
         - m1 * psi(m2 * x, n) * psi_der(m1 * x, n)
@@ -22,6 +59,21 @@ def _Bn(x, n, m1, m2):
 
 
 def an(x, y, n, m1, m2):
+    """private Bn scattering coefficient
+ 
+    Absorption and scattering of light by small particles.
+    Pg. and Equ. number.
+
+    Args:
+        x (torch.Tensor): size parameter (core)
+        y (torch.Tensor): size parameter (shell)
+        n (torch.Tensor): orders to compute
+        m1 (torch.Tensor): relative refractive index of core
+        m2 (torch.Tensor): relative refractive index of shell
+
+    Returns:
+        torch.Tensor: result
+    """
     return (
         psi(y, n) * (psi_der(m2 * y, n) - _An(x, n, m1, m2) * chi_der(m2 * y, n))
         - m2 * psi_der(y, n) * (psi(m2 * y, n) - _An(x, n, m1, m2) * chi(m2 * y, n))
@@ -32,6 +84,21 @@ def an(x, y, n, m1, m2):
 
 
 def bn(x, y, n, m1, m2):
+    """bn scattering coefficient
+
+    Absorption and scattering of light by small particles.
+    Pg. and Equ. number.
+
+    Args:
+        x (torch.Tensor): size parameter (core)
+        y (torch.Tensor): size parameter (shell)
+        n (torch.Tensor): orders to compute
+        m1 (torch.Tensor): relative refractive index of core
+        m2 (torch.Tensor): relative refractive index of shell
+
+    Returns:
+       torch.Tensor: result
+    """
     return (
         m2 * psi(y, n) * (psi_der(m2 * y, n) - _Bn(x, n, m1, m2) * chi_der(m2 * y, n))
         - psi_der(y, n) * (psi(m2 * y, n) - _Bn(x, n, m1, m2) * chi(m2 * y, n))
