83e4e38b4d32f37bee66f98ae80269a7cab7273c
Oscar Jackson <osjack.oj@gmail.com>
2025-03-26 17:45:58 +0000

    doc: examples - upadated example to use normalised parameters in optimiation

--- examples/ex_04_optimisation.py
+++ examples/ex_04_optimisation.py
@@ -3,7 +3,12 @@
 particle optimisation
 =========================
 
-Basic demonstration of unconstained particle optimisation, speatra matching example.
+Basic demonstration of particle optimisation in the visible light range.
+Farfield cross sections are optimiatised to fit a guassian curve centered 
+at 600.0nm.
+
+Core and shell refrective indexs and radii are optimised, with the materials
+limited to dielectric. 
 
 author: O. Jackson, 03/2025
 """
@@ -18,205 +23,184 @@ import numpy as np
 
 
 # %%
-# setup figure of merit
-# -------
+# setup optimiation target
+# ------------------------
 
 # - define the range of wavelengths to be incuded in optimisation.
-wl0 = torch.linspace(200, 600, 100)
+wl0 = torch.linspace(400, 800, 21)
 k0 = 2 * torch.pi / wl0
 
-
+# - for this example we target a guassian like spectra centered at 600.0nm
 def gaussian(x, mu, sig):
     return (
         1.0 / (np.sqrt(2.0 * np.pi) * sig) * np.exp(-np.power((x - mu) / sig, 2.0) / 2)
     )
+target = gaussian(wl0.numpy(), 600.0, 60.0) * 700 + 0.5
 
+target_tensor = torch.tensor(target)
 
+# - we can plot the target spectra
 plt.figure()
-target = gaussian(wl0.numpy(), 350.0, 100.0) * 800
-
 plt.plot(wl0, target, label="Target spetra.")
 plt.xlabel("$\lambda$ (nm)")
 plt.legend()
 plt.show()
 
-target_tensor = torch.tensor(target)
-
 
 # %%
-# setup particle
-# -------
+# setup particle prameter limits
+# ------------------------------
 
 # - constants
 n_env = 1.0
 
-
-# - initial guesses for pre-optimiation
-
-num = 50
-
-n_core_real_min, n_core_real_max = 0.5, 15.0
-n_core_imag_min, n_core_imag_max = 0.01, 2.0
-n_shell_real_min, n_shell_real_max = 0.5, 15.0
-n_shell_imag_min, n_shell_imag_max = 0.01, 2.0
-
-r_core_min, r_core_max = 10.0, 50.0
-r_shell_min, r_shell_max = 10.0, 100.0
-
-
-# n_core_array = np.random.uniform(low=0.5, high=15.0, size=num) + np.random.uniform(low=0.01, high=2.0, size=num)*1j
-# n_shell_array = np.random.uniform(low=0.5, high=15.0, size=num) + np.random.uniform(low=0.01, high=2.0, size=num)*1j
-# r_core_arr = np.random.uniform(low=1.0, high=50.0, size=num)
-# r_shell_arr = r_core_arr + np.random.uniform(low=1.0, high=50.0, size=num)
-
-
-# # %%
-# # plot efficiency spectra
-# # -------
-# # Calculate fom of initial guesses to find starting point.
-# plt.figure()
-# best = 1e5, 0
-# for i in range(num):
-#     cs = pmd.farfield.cross_sections(k0, r_core_arr[i], (n_core_array[i])**2, r_shell_arr[i], (n_shell_array[i])**2, eps_env= 1.44)
-#     plt.plot(cs["wavelength"], cs["q_sca"].detach())
-#     pre_loss = torch.nn.functional.mse_loss(target_tensor, cs["q_sca"])
-#     if best[0] > pre_loss:
-#         best = pre_loss, i
-#         print("new best loss: ", pre_loss.item())
-
-# plt.xlabel("wavelength (nm)")
-# plt.ylabel("Efficiency")
-# plt.tight_layout()
-# plt.show()
-
-
-# # %%
-# # plot results of pre-optimiation
-# # -------
-# print(f"best fom: {best[0]}, with rc: {r_core_arr[best[1]]}, rs: {r_shell_arr[best[1]]}")
-
-# plt.figure()
-# cs = pmd.farfield.cross_sections(k0, r_core_arr[best[1]], (n_core_array[i])**2, r_shell_arr[i], (n_shell_array[i])**2)
-# plt.plot(cs["wavelength"], cs["q_sca"], label="$Q_{sca}$")
-# plt.plot(cs["wavelength"], target_tensor, label="$Q_{sca}^{target}$")
-# plt.legend()
-# plt.xlabel("wavelength (nm)")
-# plt.ylabel("Efficiency")
-# plt.tight_layout()
-# plt.show()
-
+# - set limits to particle's properties, in this example we limit to dielectric materials
+lim_r = torch.as_tensor([10, 100], dtype=torch.double)
+lim_n_re = torch.as_tensor([1, 4.5], dtype=torch.double)
+lim_n_im = torch.as_tensor([0, 0.1], dtype=torch.double)
 
 # %%
-# optimiation config
-# ------------------
-# - get parameter tensors ready for optimisation
+# normalization helper
+# --------------------
+# we let the optimizer work on normalized parameters which we pass through a sigmoid. 
+# This is a straightforward way to implement box boundaries for the optimization variables.
 
-r_c = torch.tensor(np.random.random(), requires_grad=True, dtype=torch.double)
-r_s = torch.tensor(np.random.random(), requires_grad=True, dtype=torch.double)
+def normalize_and_convert_to_physical(k0, r_opt, n_opt):
+    # sigmoid: constrain to a given parameter range
+    sigmoid = torch.nn.Sigmoid()
+    r_c_n, d_s_n = sigmoid(r_opt)
+    n_c_re_n, n_s_re_n, n_c_im_n, n_s_im_n = sigmoid(n_opt)
 
-n_c = torch.tensor(
-    np.random.random() + np.random.random() * 1j,
-    requires_grad=True,
-    dtype=torch.cdouble,
-)
-n_s = torch.tensor(
-    np.random.random() + np.random.random() * 1j,
-    requires_grad=True,
-    dtype=torch.cdouble,
-)
-
-# - define optimister
-
-optimizer = torch.optim.LBFGS([r_c, n_c, r_s, n_s], lr=0.9, max_iter=5, history_size=7)
-
-
-# %%
-# optimiation loop
-# ------------------
-# define losses and create optimiation loop
-
-max_iter = 100
-losses = []  # Array to store loss data
-
-
-def scale_back(normalized_value, min_value, max_value):
-    return (
-        normalized_value * (max_value - min_value)
-        + min_value
+    # scale parameters to physical units
+    # size parameters
+    r_c = r_c_n * (lim_r.max() - lim_r.min()) + lim_r.min()
+    d_s = d_s_n * (lim_r.max() - lim_r.min()) + lim_r.min()
+    r_s = r_c + d_s
+    
+    # core and shell complex ref. index
+    n_c = (n_c_re_n * (lim_n_re.max() - lim_n_re.min()) + lim_n_re.min()) + 1j * (
+        n_c_im_n * (lim_n_im.max() - lim_n_im.min()) + lim_n_im.min()
+    )
+    n_s = (n_s_re_n * (lim_n_re.max() - lim_n_re.min()) + lim_n_re.min()) + 1j * (
+        n_s_im_n * (lim_n_im.max() - lim_n_im.min()) + lim_n_im.min()
     )
 
+    args = (k0, r_c, n_c**2, r_s, n_s**2)
+    return args
 
+
+# %%
+# optimisation config
+# ------------------
+# random initial guesses. here we impliment a simple global 
+# search to improve the gradient optimization.
+
+pre_opt = 500
+# array to hold best initial guess
+best = [100.0, np.random.random(2), np.random.random(4)]
+
+# contains 2 values: radius of core and thickness of shell
+r_opt_arr = np.random.random((2, pre_opt))
+# contains 4 values: real and imag parts of core and shell ref.index
+n_opt_arr = np.random.random((4, pre_opt))
+
+for i in range(pre_opt):
+    args = normalize_and_convert_to_physical(
+        k0,
+        torch.tensor(r_opt_arr[:,i], dtype=torch.double), 
+        torch.tensor(n_opt_arr[:,i], dtype=torch.double),
+        )
+
+    # evaluate Mie
+    result_mie = pmd.farfield.cross_sections(*args)["q_sca"]
+    # get loss, MSE comparing target with current spectra
+    loss = torch.nn.functional.mse_loss(target_tensor, result_mie)
+    # update best initial guess
+    if loss < best[0]:
+        best[0] = loss
+        best[1] = r_opt_arr[:,i]
+        best[2] = n_opt_arr[:,i]
+        print("new best with loss:", loss.item())
+
+
+# %%
+# setup tensors for optimisation
+# ------------------------------
+# initialise tensors for optimisation using the best guess found in the
+# global search. NOTE. Run from this cell down if you want to experiment  
+# with optimiser hyperparametrs.
+
+r_opt = torch.tensor(best[1], requires_grad=True, dtype=torch.double)
+n_opt = torch.tensor(best[2], requires_grad=True, dtype=torch.double)
+
+# %%
+# optimisation loop
+# ------------------
+# define losses, create and run optimization loop. In this example a
+# LBFGS optimisier is used.
+
+# - define optimiser and hyperparameters
+optimizer = torch.optim.LBFGS([r_opt, n_opt], lr=0.25, max_iter=10, history_size=7)
+max_iter = 40
+
+# for LFBGS: closure 
 def closure():
     optimizer.zero_grad()  # Reset gradients
+    
+    # scale parameters to physical units
+    args = normalize_and_convert_to_physical(k0, r_opt, n_opt)
 
-    # scale parameters to make physical
-    args = (
-        k0,
-        scale_back(r_c, r_core_min, r_core_max),
-        (
-            scale_back(n_c.real, n_core_real_min, n_core_real_max)
-            + 1j * scale_back(n_c.imag, n_core_imag_min, n_core_imag_max)
-        )** 2,
-        scale_back(r_c, r_core_min, r_core_max) + scale_back(r_s, r_shell_min, r_shell_max),
-        (
-            scale_back(n_s.real, n_shell_real_min, n_shell_real_max)
-            + 1j * scale_back(n_s.imag, n_shell_imag_min, n_shell_imag_max)
-        )** 2,
-    )
+    # evaluate Mie
+    result_mie = pmd.farfield.cross_sections(*args)["q_sca"]
+    loss = torch.nn.functional.mse_loss(target_tensor, result_mie)
 
-    iteration_n = pmd.farfield.cross_sections(*args)["q_sca"]
-    loss = torch.nn.functional.mse_loss(target_tensor, iteration_n)
-
-    loss.backward()  # Compute gradients
+    loss.backward()  # Compute gradients (using AutoDiff)
     return loss
 
 
+# main loop
+losses = []  # Array to store loss data
 for o in range(max_iter + 1):
-
-    # r_c = torch.nn.functional.sigmoid(r_c)
 
     loss = optimizer.step(closure)  # LBFGS requires closure
 
     losses.append(loss.item())  # Store loss value
 
     if o % 5 == 0:
-        print(o, loss.item())
-        print(r_c.item(), r_s.item())
-        print(n_c.item(), n_s.item())
+        print(" --- iter {}: loss={:.2f}".format(o, loss.item()))
+        args = normalize_and_convert_to_physical(k0, r_opt, n_opt)
+        print("     r_core  = {:.1f}nm".format(args[1]))
+        print("     r_shell = {:.1f}nm".format(args[3]))
+        print("     n_core  = {:.2f}".format(torch.sqrt(args[2])))
+        print("     n_shell = {:.2f}".format(torch.sqrt(args[4])))
 
 
 # %%
-# view optimised results
+# optimisation results
+# --------------------
+# view optimised speactra and corresponding particle parameters.
 
 # - plot optimised spectra against target spectra
+wl0_eval = torch.linspace(400, 800, 151)
+k0_eval = 2 * torch.pi / wl0_eval
+args = normalize_and_convert_to_physical(k0_eval, r_opt, n_opt)
 
-args = (
-    scale_back(r_c, r_core_min, r_core_max),
-    (
-        scale_back(n_c.real, n_core_real_min, n_core_real_max)
-        + 1j * scale_back(n_c.imag, n_core_imag_min, n_core_imag_max)
-    )
-    ** 2,
-    scale_back(r_c, r_core_min, r_core_max) + scale_back(r_s, r_shell_min, r_shell_max),
-    (
-        scale_back(n_s.real, n_shell_real_min, n_shell_real_max)
-        + 1j * scale_back(n_s.imag, n_shell_imag_min, n_shell_imag_max)
-    )
-    ** 2,
-)
+cs_opt = pmd.farfield.cross_sections(*args)
 
-
-cs_opt = pmd.farfield.cross_sections(k0, *args)
-print("final:", [f"{d.detach().numpy()}" for d in args])
 plt.figure()
 plt.plot(cs_opt["wavelength"], cs_opt["q_sca"].detach(), label="$Q_{sca}^{optim}$")
-plt.plot(
-    cs_opt["wavelength"], target_tensor, label="$Q_{sca}^{target}$", linestyle="--"
-)
+plt.plot(wl0, target_tensor, label="$Q_{sca}^{target}$", linestyle="--")
 plt.xlabel("wavelength (nm)")
 plt.ylabel("Efficiency")
 plt.legend()
 plt.tight_layout()
 plt.show()
+
+# - print optimun parameters
+print("optimum:")
+print(" r_core  = {:.1f}nm".format(args[1]))
+print(" r_shell = {:.1f}nm".format(args[3]))
+print(" n_core  = {:.2f}".format(torch.sqrt(args[2])))
+print(" n_shell = {:.2f}".format(torch.sqrt(args[4])))
 
 # %%
--- testing/ex_03_optimisation_proposition_peter.py
+++ testing/ex_03_optimisation_proposition_peter.py
@@ -3,7 +3,12 @@
 particle optimisation
 =========================
 
-Basic demonstration of unconstained particle optimisation, speatra matching example.
+Basic demonstration of particle optimisation in the visible light range.
+Farfield cross sections are optimiatised to fit a guassian curve centered 
+at 600.0nm.
+
+Core and shell refrective indexs and radii are optimised, with the materials
+limited to dielectric. 
 
 author: O. Jackson, 03/2025
 """
@@ -18,66 +23,47 @@ import numpy as np
 
 
 # %%
-# setup figure of merit
-# -------
+# setup optimiation target
+# ------------------------
 
 # - define the range of wavelengths to be incuded in optimisation.
 wl0 = torch.linspace(400, 800, 21)
 k0 = 2 * torch.pi / wl0
 
-
+# - for this example we target a guassian like spectra centered at 600.0nm
 def gaussian(x, mu, sig):
     return (
         1.0 / (np.sqrt(2.0 * np.pi) * sig) * np.exp(-np.power((x - mu) / sig, 2.0) / 2)
     )
+target = gaussian(wl0.numpy(), 600.0, 60.0) * 700 + 0.5
 
+target_tensor = torch.tensor(target)
 
+# - we can plot the target spectra
 plt.figure()
-target = gaussian(wl0.numpy(), 600.0, 60.0) * 800
-
 plt.plot(wl0, target, label="Target spetra.")
 plt.xlabel("$\lambda$ (nm)")
 plt.legend()
 plt.show()
 
-target_tensor = torch.tensor(target)
-
 
 # %%
-# setup particle
-# -------
+# setup particle prameter limits
+# ------------------------------
 
 # - constants
 n_env = 1.0
 
-
-# - initial guesses for pre-optimiation
-
+# - set limits to particle's properties, in this example we limit to dielectric materials
 lim_r = torch.as_tensor([10, 100], dtype=torch.double)
 lim_n_re = torch.as_tensor([1, 4.5], dtype=torch.double)
 lim_n_im = torch.as_tensor([0, 0.1], dtype=torch.double)
-
-
-
-# %%
-# optimization config
-# ------------------
-# random initial guesses. Note that adopting a better strategy here may significantly improve gradient optimization.
-
-# contains 2 values: radius of core and thickness of shell
-r_opt = torch.tensor(np.random.random(2), requires_grad=True, dtype=torch.double)
-
-# contains 4 values: real and imag parts of core and shell ref.index
-n_opt = torch.tensor(np.random.random(4), requires_grad=True, dtype=torch.double)
-
-
 
 # %%
 # normalization helper
 # --------------------
 # we let the optimizer work on normalized parameters which we pass through a sigmoid. 
 # This is a straightforward way to implement box boundaries for the optimization variables.
-
 
 def normalize_and_convert_to_physical(k0, r_opt, n_opt):
     # sigmoid: constrain to a given parameter range
@@ -103,16 +89,59 @@ def normalize_and_convert_to_physical(k0, r_opt, n_opt):
     return args
 
 
+# %%
+# optimisation config
+# ------------------
+# random initial guesses. here we impliment a simple global 
+# search to improve the gradient optimization.
+
+pre_opt = 500
+# array to hold best initial guess
+best = [100.0, np.random.random(2), np.random.random(4)]
+
+# contains 2 values: radius of core and thickness of shell
+r_opt_arr = np.random.random((2, pre_opt))
+# contains 4 values: real and imag parts of core and shell ref.index
+n_opt_arr = np.random.random((4, pre_opt))
+
+for i in range(pre_opt):
+    args = normalize_and_convert_to_physical(
+        k0,
+        torch.tensor(r_opt_arr[:,i], dtype=torch.double), 
+        torch.tensor(n_opt_arr[:,i], dtype=torch.double),
+        )
+
+    # evaluate Mie
+    result_mie = pmd.farfield.cross_sections(*args)["q_sca"]
+    # get loss, MSE comparing target with current spectra
+    loss = torch.nn.functional.mse_loss(target_tensor, result_mie)
+    # update best initial guess
+    if loss < best[0]:
+        best[0] = loss
+        best[1] = r_opt_arr[:,i]
+        best[2] = n_opt_arr[:,i]
+        print("new best with loss:", loss.item())
+
 
 # %%
-# optimization loop
+# setup tensors for optimisation
+# ------------------------------
+# initialise tensors for optimisation using the best guess found in the
+# global search. NOTE. Run from this cell down if you want to experiment  
+# with optimiser hyperparametrs.
+
+r_opt = torch.tensor(best[1], requires_grad=True, dtype=torch.double)
+n_opt = torch.tensor(best[2], requires_grad=True, dtype=torch.double)
+
+# %%
+# optimisation loop
 # ------------------
-# define losses and create optimization loop
+# define losses, create and run optimization loop. In this example a
+# LBFGS optimisier is used.
 
-
-# - define optimiser
+# - define optimiser and hyperparameters
 optimizer = torch.optim.LBFGS([r_opt, n_opt], lr=0.25, max_iter=10, history_size=7)
-max_iter = 100
+max_iter = 40
 
 # for LFBGS: closure 
 def closure():
@@ -125,7 +154,7 @@ def closure():
     result_mie = pmd.farfield.cross_sections(*args)["q_sca"]
     loss = torch.nn.functional.mse_loss(target_tensor, result_mie)
 
-    loss.backward()  # Compute gradients
+    loss.backward()  # Compute gradients (using AutoDiff)
     return loss
 
 
@@ -133,14 +162,11 @@ def closure():
 losses = []  # Array to store loss data
 for o in range(max_iter + 1):
 
-    # r_c = torch.nn.functional.sigmoid(r_c)
-
     loss = optimizer.step(closure)  # LBFGS requires closure
 
     losses.append(loss.item())  # Store loss value
 
     if o % 5 == 0:
-    # if 1:
         print(" --- iter {}: loss={:.2f}".format(o, loss.item()))
         args = normalize_and_convert_to_physical(k0, r_opt, n_opt)
         print("     r_core  = {:.1f}nm".format(args[1]))
@@ -150,7 +176,9 @@ for o in range(max_iter + 1):
 
 
 # %%
-# view optimised results
+# optimisation results
+# --------------------
+# view optimised speactra and corresponding particle parameters.
 
 # - plot optimised spectra against target spectra
 wl0_eval = torch.linspace(400, 800, 151)
@@ -158,20 +186,21 @@ k0_eval = 2 * torch.pi / wl0_eval
 args = normalize_and_convert_to_physical(k0_eval, r_opt, n_opt)
 
 cs_opt = pmd.farfield.cross_sections(*args)
-print("final:", [f"{d.detach().numpy()}" for d in args])
+
 plt.figure()
 plt.plot(cs_opt["wavelength"], cs_opt["q_sca"].detach(), label="$Q_{sca}^{optim}$")
-plt.plot(
-    wl0, target_tensor, label="$Q_{sca}^{target}$", linestyle="--"
-)
+plt.plot(wl0, target_tensor, label="$Q_{sca}^{target}$", linestyle="--")
 plt.xlabel("wavelength (nm)")
 plt.ylabel("Efficiency")
 plt.legend()
 plt.tight_layout()
 plt.show()
 
+# - print optimun parameters
 print("optimum:")
 print(" r_core  = {:.1f}nm".format(args[1]))
 print(" r_shell = {:.1f}nm".format(args[3]))
 print(" n_core  = {:.2f}".format(torch.sqrt(args[2])))
 print(" n_shell = {:.2f}".format(torch.sqrt(args[4])))
+
+# %%
