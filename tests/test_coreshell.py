1bdf0aa7bdb97a3432b91c2fa96ce289314fce06
Peter R. Wiecha <pwiecha@laas.fr>
2025-10-15 18:11:02 +0200

    unittest: Mie Coeff. against treams

--- tests/test_coreshell.py
+++ tests/test_coreshell.py
@@ -1,4 +1,5 @@
 import unittest
+import warnings
 
 import torch
 import numpy as np
@@ -166,6 +167,114 @@ class TestCoefficientsForwards(unittest.TestCase):
             torch.testing.assert_close(
                 result_scipy[1], result_ad["b_n"], rtol=tol, atol=tol
             )  # bn
+
+
+class TestCoefficientsForwardsTreams(unittest.TestCase):
+    def test_mie_coeff_against_treams(self):
+        # setup a core-shell test case
+
+        # - config
+        N_wl = 2
+        wl0 = torch.linspace(500, 1500, N_wl)
+        k0 = 2 * torch.pi / wl0
+
+        r_core = 200.0
+        r_shell = 420.0
+        n_core = 0.5 + 5.1j
+        n_shell = 4.5  # caution: metal-like shells can become unstable
+        mat_core = pmd.materials.MatConstant(n_core**2)
+        mat_shell = pmd.materials.MatConstant(n_shell**2)
+
+        n_env = 1.0
+
+        n_max = 10  # max Mie order
+
+        # Mie from `treams`
+        # https://github.com/tfp-photonics/treams
+        try:
+            import treams
+        except ImportError:
+            warnings.warn(
+                "`treams` seems not installed. "
+                + "Skipping Mie coefficient test vs `treams`."
+            )
+            return
+
+        def mie_ab_sphere_treams(
+            k0: np.ndarray, radii: list, materials: list, n_env, n_max
+        ):
+            """Mie scattering via package `treams`, for vacuum environment"""
+            assert len(radii) == len(materials)
+
+            # embedding medium: vacuum
+            eps_env = n_env**2
+
+            # main Mie extraction and setup
+            a_n = np.zeros((len(k0), n_max), dtype=np.complex128)
+            b_n = np.zeros_like(a_n)
+            for i_wl, _k0 in enumerate(k0):
+
+                # core and shell materials
+                mat_treams = []
+                for eps_mat in materials:
+                    mat_treams.append(treams.Material(eps_mat))
+
+                # treams convention: environment material last
+                mat_treams.append(treams.Material(eps_env))
+
+                for n in range(1, n_max + 1):
+                    miecoef = treams.coeffs.mie(
+                        n, _k0 * np.array(radii), *zip(*mat_treams)
+                    )
+                    a_n[i_wl, n - 1] = -miecoef[0, 0] - miecoef[0, 1]
+                    b_n[i_wl, n - 1] = -miecoef[0, 0] + miecoef[0, 1]
+
+            # scattering
+            Qs_mie = np.zeros(len(k0))
+            for n in range(n_max):
+                Qs_mie += (2 * (n + 1) + 1) * (
+                    np.abs(a_n[:, n]) ** 2 + np.abs(b_n[:, n]) ** 2
+                )
+            Qs_mie *= 2 / (k0 * n_env * max(radii)).real ** 2
+
+            return dict(
+                a_n=a_n,
+                b_n=b_n,
+                q_sca=Qs_mie,
+                wavelength=2 * np.pi / k0,
+            )
+
+        cs_treams = mie_ab_sphere_treams(
+            k0=k0.numpy(),
+            radii=[r_core, r_shell],
+            materials=[n_core**2, n_shell**2],
+            n_env=n_env,
+            n_max=n_max,
+        )
+        a_t = np.moveaxis(cs_treams["a_n"], 0, 1)
+        b_t = np.moveaxis(cs_treams["b_n"], 0, 1)
+
+        # pymiediff
+        mie_ceof = pmd.coreshell.mie_coefficients(
+            k0,
+            r_c=r_core,
+            eps_c=n_core**2,
+            r_s=r_shell,
+            eps_s=n_shell**2,
+            eps_env=n_env**2,
+            backend="torch",
+            n_max=n_max,
+        )
+
+        a_pmd = mie_ceof["a_n"][:, 0]  # first particle
+        b_pmd = mie_ceof["b_n"][:, 0]  # first particle
+
+        a_p = a_pmd.cpu().detach().numpy()
+        b_p = b_pmd.cpu().detach().numpy()
+
+        # compare
+        np.testing.assert_allclose(a_p, a_t, atol=1e-5, rtol=1e-5)
+        np.testing.assert_allclose(b_p, b_t, atol=1e-5, rtol=1e-5)
 
 
 if __name__ == "__main__":
